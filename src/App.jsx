df1985be34f6017b5bd8c4b9f51a6a2440825ad4
Juan Pablo Miranda <jpablomiranda.21@gmail.com>
2023-08-25 12:23:03 -0300

    estructura basica contacto

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,15 @@ import Footer from './components/common/Footer'
 import Menu from './components/common/Menu'
 import PaginaPrincipal from './components/views/PaginaPrincipal';
 import Error404 from './components/views/Error404';
+import Contacto from './components/views/Contacto';
 
 function App() {
   return (
     <>
       <Menu></Menu>
       {/* <PaginaPrincipal></PaginaPrincipal> */}
-      <Error404></Error404>
+      {/* <Error404></Error404> */}
+      <Contacto></Contacto>
       <Footer></Footer>
     </>
   )
